a8a6da5277ea20db02f81150a59f96c6ca29aee4
soethihazaw <soeNthe@gamil.com>
2019-12-15 21:08:59 +0630

    HelloWorld v2

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+ #1 2 3 4 5
+
+# this is the first comment
+
+spam = 5 + 6 - 3 / 4 * 2 # and this is the second comment
+		 # ... and now a third!
+text = "#This is not a comment because it's inside quotes."
+
+print(spam)
+print(text)
